41a5d2c26495e3a1527643e92d51c86e00059259
Raiden2301 <mireabogdancristian@gmail.com>
2018-03-28 00:02:11 +0300

    Interfata e aproape gata

--- app/src/main/java/com/bogdancristian/tictactoe/MainActivity.java
+++ app/src/main/java/com/bogdancristian/tictactoe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bogdancristian.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openGrid1(View view){
+        Intent intent = new Intent (this , GridActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGrid2(View view){
+        Intent intent = new Intent(this , Grid2Activity.class);
+        startActivity(intent);
+    }
+
+    public void closeAplication(View view){
+        finish();
+        System.exit(0);
     }
 }
