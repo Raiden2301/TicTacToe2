f7f3cdb5e150ccb34f00b9f54c64c5b2fe4decab
Raiden2301 <mireabogdancristian@gmail.com>
2018-04-02 22:47:38 +0300

    02.04.2018 Am rezolvat tema aplicatiei. Sound button nu functioneaza inca.

--- app/src/main/java/com/bogdancristian/tictactoe/MainActivity.java
+++ app/src/main/java/com/bogdancristian/tictactoe/MainActivity.java
@@ -1,11 +1,15 @@
 package com.bogdancristian.tictactoe;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,18 +17,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openGrid1(View view){
-        Intent intent = new Intent (this , GridActivity.class);
+    public void openGrid1(View view) {
+        Intent intent = new Intent(this, GridActivity.class);
         startActivity(intent);
     }
 
-    public void openGrid2(View view){
-        Intent intent = new Intent(this , Grid2Activity.class);
+    public void openGrid2(View view) {
+        Intent intent = new Intent(this, Grid2Activity.class);
         startActivity(intent);
     }
 
-    public void closeAplication(View view){
+    public void closeAplication(View view) {
         finish();
         System.exit(0);
     }
+
+//    boolean sound = true;
+//    final ImageButton btn = (ImageButton)findViewById(R.id.btnSound);
+//
+//    private View.OnClickListener biscuit = new View.OnClickListener() {
+//         public void onClick(View v) {
+//        if(sound){
+//            btn.setBackgroundResource(R.drawable.off);
+//
+//        }else{
+//            btn.setBackgroundResource(R.drawable.on);
+//        }
+//        sound =! sound;
+//        }
+//    };
+
+
+
 }
+
